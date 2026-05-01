252ef390aa818865498f5a4792da25ee2241b73c
Kévin Lefèvre <1300874+klefevre@users.noreply.github.com>
2026-05-01 16:43:09 +0200

    nix flake updatee

--- home/shells/fish.nix
+++ home/shells/fish.nix
@@ -8,6 +8,7 @@
       fish_add_path /opt/homebrew/bin
       fish_add_path ~/mutable_node_modules/bin
       fish_add_path ~/.local/bin
+      fish_add_path ~/.opencode/bin
 
       function fish_greeting
         fortune -a | cowsay
